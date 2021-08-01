f5eac3678dcbc4515b8711296cc289dc1e3c675d
aemtechnologies <65249478+aemtechnologies@users.noreply.github.com>
2021-08-01 07:59:30 +0530

    Update app.py

--- gke-lab-01/app.py
+++ gke-lab-01/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def showPinehead():
-    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from AEM Technologies!</h1><img src='https://IP/aemtechlogo.png' width='40%' alt='Tuhin @ AEM Technologies'></div>"
+    #html = "<div style='text-align:center;margin:20px;'><h1>Greetings from AEM Technologies!</h1><img src='https://IP/aemtechlogo.png' width='40%' alt='Tuhin @ AEM Technologies'></div>"
+    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from AEM Technologies!</h1>'</div>"
     return html
 
 if __name__ == "__main__":
